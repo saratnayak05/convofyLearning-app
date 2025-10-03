f3b98e10861bd2fa3375ee31f3747b894ac2f29e
saratnayak05 <saratnayak0808@gmail.com>
2025-10-04 03:04:10 +0530

    signupController auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,66 @@
-export async function signup(req,res){
-    res.send("signup route");
-};
+import User from '../models/User.js'
+import jwt  from  'jsonwebtoken'
 
-export async function login(req,res){
-    res.send("login route");
-};
+export async function signup(req, res) {
+  const { email, password, fullName } = req.body;
 
-export function logout(req,res){
-    res.send("logout route");
-};
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400), json({ message: "All feilds are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+     
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({message: "Email alredy exists, please use a diffrent one"});
+    }
+
+    const idx = Math.floor(Math.random() *  100) + 1;// generate a num between 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`
+
+    const newUser = new User.create({
+        email,
+        fullName,
+        password,
+        profiilePic: randomAvatar,
+    })
+
+     //TODO: CREATE THE USER IN STREAM AS WELL
+
+    const token = jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY,{
+        expiresIn: "7d"
+    })
+
+    res.cookie("jwt",token,{
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true, // prevent xss attacks
+        sameSite: "strict", //prevent csrf attacks
+        secure: process.envNODE_ENV === "production"
+    })
+
+    res.status(201).json({success:true, user:newUser})
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    res.status(500).json({ message: "Internal Server Error"});
+  }
+}
+
+export async function login(req, res) {
+  res.send("login route");
+}
+
+export function logout(req, res) {
+  res.send("logout route");
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
-
+app.use(express.json());
 
 app.use("/api/auth/", authRoutes)
 
