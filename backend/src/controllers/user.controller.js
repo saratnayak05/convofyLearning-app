367f4b8f256435562e0a33f5a121e1e4aab7004a
saratnayak05 <saratnayak0808@gmail.com>
2025-10-09 18:21:27 +0530

    add friends api send request routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import FriendRequest from "../models/FriendRequest.js";
 
 
 export async function getRecommendedUsers(req,res){
@@ -30,5 +31,47 @@ export async function getMyFriends(req,res){
     } catch (error) {
         console.error("Error in getMyFriends controller", error.message);
         res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export async function sendFriendRequest(req, res) {
+    try {
+        const myId =req.user.id;
+        const { id:recipientId } = req.params
+
+        //prevent sedning req t yourself
+        if( myId === recipientId) {
+            return res.status(400).json({ message: "You can't send friend request to yourself" });
+        }
+
+        const recipient = await User.findById(recipientId)
+        if(!recipient){
+            return res.status(404).json({ message: "Recipient not found" });
+        }
+       
+        if(recipient.friends.includes(myId)) {
+            return res.status(400).json({ message: "You are already friends with this user" });
+        }
+
+        const existingRequest = await FriendRequest.findOne({
+            $or: [
+                { sender: myId, recipient: recipientId },
+                { sender: recipientId, recipient: myId },
+            ]
+        })
+
+        if(existingRequest){
+            res.status(400).json({ message: "A friend request already exists between you and this user" });
+        }
+
+        const FriendRequest = await FriendRequest.create({
+            sender: myId,
+            recipient: recipientId,
+        });
+
+        res.status(201).json(FriendRequest);
+    } catch (error) {
+        console.error("Error in sendFriendRequest controller", error.message);
+        res.status(500),json({ message: "Internal Server Error" });
     }
 }
