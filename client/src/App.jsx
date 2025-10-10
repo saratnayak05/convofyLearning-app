62b7f1648c0761ce2436430382f951201d50f5b3
saratnayak05 <saratnayak0808@gmail.com>
2025-10-10 12:45:05 +0530

    setup tanstack/react-query axios manage cors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,47 @@
-import {Routes, Route} from "react-router"
+import { Routes, Route } from "react-router";
 
-import HomePage from "./pages/HomePage.jsx"
-import LoginPage from "./pages/LoginPage.jsx"
-import SignUpPage from "./pages/SignUpPage.jsx"
-import NotificationsPage from "./pages/NotificationPage.jsx"
-import CallPage from "./pages/CallPage.jsx"
-import ChatPage from "./pages/ChatPage.jsx"
-import OnboardingPage from "./pages/OnboardingPage.jsx"
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import NotificationsPage from "./pages/NotificationPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
 
-import { Toaster }  from "react-hot-toast"
-  
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 
 const App = () => {
+  //tanstack query ----
+
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+  });
+
+  console.log(data);
+
   return (
-    <div className='h-screen' data-theme="night">
-       
+    <div className="h-screen" data-theme="night">
       <Routes>
-
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/signup" element={<SignUpPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/notifications" element={<NotificationsPage/>} />
-        <Route path="/call" element={<CallPage/>} />
-        <Route path="/chat" element={<ChatPage/>} />
-        <Route path="/onboarding" element={<OnboardingPage/>} />
-
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
-      
-      <Toaster/>
-    </div>
-  )
-}
 
-export default App
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-import { BrowserRouter } from "react-router"
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
