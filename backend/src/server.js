7db020bb6ae35ebb71b0213a9fcb15b0fc23592b
saratnayak05 <saratnayak0808@gmail.com>
2025-10-22 01:39:58 +0530

    deploy convofy

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./Routes/auth.route.js";
 import userRoutes from "./Routes/user.route.js";
@@ -11,6 +12,8 @@ import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -24,6 +27,15 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req,res)=>{
+   res.sendFile(path.join(__dirname, "../client" , "dist" , "index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`server started ${PORT}`);
