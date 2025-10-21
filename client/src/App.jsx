4efac7b853f66a0e69e52dec7366c7aa860794a0
saratnayak05 <saratnayak0808@gmail.com>
2025-10-22 00:10:36 +0530

    deploy setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 
 import { Toaster } from "react-hot-toast";
-import PageLoader from "./components/pageLoader.jsx";
+import PageLoader from "./components/PageLoader.jsx";
 import useAuthUser from "./hooks/useAuthUser.js";
 import Layout from "./components/Layout.jsx";
 import { useThemeStore } from "./store/useThemeStore.js";
